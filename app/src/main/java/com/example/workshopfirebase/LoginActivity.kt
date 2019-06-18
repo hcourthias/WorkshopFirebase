47b77a443f7fe391fcb698e0735bfee544741455
Hugo Courthias <hugo.courthias@gmail.com>
2019-06-18 13:45:17 +0200

    ADD: Login, SignUp & ForgotPassword UI

--- app/src/main/java/com/example/workshopfirebase/LoginActivity.kt
+++ app/src/main/java/com/example/workshopfirebase/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.example.workshopfirebase
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    //Add FirebaseAuth variable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signup_button.setOnClickListener{ startActivity(Intent(this, SignUpActivity::class.java)) }
+        forgotpass_text.setOnClickListener{ startActivity(Intent(this, ForgotPassActivity::class.java)) }
+
     }
+
+
 }
